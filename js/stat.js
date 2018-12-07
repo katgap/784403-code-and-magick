463acc93ee322049dc0ee58c413d35d9cc3d7110
Екатерина <k-gaponova@mail.ru>
2018-12-07 16:52:31 +0300

    Исправление стилистических ошибок

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,10 @@
-var canvas =  document.getElementById('canvas');
+'use strict';
+
+var canvas = document.getElementById('canvas');
 
 var ctx = canvas.getContext('2D');
 
-var renderStatistics = function(ctx, names, times) {
+var renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
@@ -12,33 +14,33 @@ var renderStatistics = function(ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'rgb(0, 0, 0)';
 
-  var title = ['Ура вы победили!','Список результатов:'];
+  var title = ['Ура вы победили!', 'Список результатов:'];
   var lineSpacing = 0;
-  for (var i = 0; i < title.length; i++) {
-    ctx.fillText(title[i], 150, 30 + lineSpacing);
+  for (var indexTitle = 0; indexTitle < title.length; indexTitle++) {
+    ctx.fillText(title[indexTitle], 150, 30 + lineSpacing);
     lineSpacing += 21;
   }
 
   var maxTime = times[0];
-  for (var i = 1; i < times.length; i++) {
-    if(times[i] > maxTime) {
-      maxTime = times[i];
+  for (var indexTimes = 1; indexTimes < times.length; indexTimes++) {
+    if (times[indexTimes] > maxTime) {
+      maxTime = times[indexTimes];
     }
-  };
+  }
   maxTime = Math.round(maxTime);
 
   var distanceBetweenColumns = 0;
   var heightOfColumn = 0;
-  for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = 'rgba(15, 7, 133, '+ Math.ceil(Math.random() * 10)/10+ ')';
-    if (names[i]==='Вы') {
+  for (var indexNames = 0; indexNames < names.length; indexNames++) {
+    ctx.fillStyle = 'rgba(15, 7, 133, ' + Math.ceil(Math.random() * 10) / 10 + ')';
+    if (names[indexNames] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
-    heightOfColumn = (Math.round(times[i]) * 150) / maxTime;
-    ctx.fillRect(150 + distanceBetweenColumns, (125-heightOfColumn)+125, 40, heightOfColumn);
+    heightOfColumn = (Math.round(times[indexNames]) * 150) / maxTime;
+    ctx.fillRect(150 + distanceBetweenColumns, (125 - heightOfColumn) + 125, 40, heightOfColumn);
     ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.fillText(Math.round(times[i]), 150 + distanceBetweenColumns, 90);
-    ctx.fillText(names[i], 150 + distanceBetweenColumns, 270);
+    ctx.fillText(Math.round(times[indexNames]), 150 + distanceBetweenColumns, 90);
+    ctx.fillText(names[indexNames], 150 + distanceBetweenColumns, 270);
     distanceBetweenColumns += 50;
   }
 };
